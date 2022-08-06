d92fd1b3bb71bb1b98b6b272975588ba2dce8f8e
Cassio Oliveira Silva <cos@vortx.com.br>
2022-08-06 20:17:40 -0300

    chore: add toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Routers from './routes';
+import { apolloClient } from './services/clients/apolloClient';
 import GlobalStyle from './styles/global';
+import theme from './theme';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Routers />
-      <ToastContainer theme="dark" />
-    </BrowserRouter>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routers />
+        <ToastContainer
+          toastStyle={{
+            backgroundColor: theme.colors.gray[800],
+            color: theme.colors.txt,
+          }}
+        />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
