bb58ffce4f1ad6a573edbf42aa776e764cacc14c
Cassio Oliveira Silva <cos@vortx.com.br>
2022-08-12 18:36:56 -0300

    feat: add an icon in NoContent componente

--- src/components/NoContent/index.tsx
+++ src/components/NoContent/index.tsx
@@ -1,5 +1,11 @@
+import { TbMoodEmpty } from 'react-icons/tb';
 import { Container } from './style';
 
 export default function NoContent() {
-  return <Container>Não existem registros.</Container>;
+  return (
+    <Container>
+      <TbMoodEmpty size={50} />
+      <p>Você não tem nenhuma transação cadastrada.</p>
+    </Container>
+  );
 }
