adc71162c168cd1cfacb6410d7770a0784d82892
Cassio Oliveira Silva <cos@vortx.com.br>
2022-08-12 18:37:24 -0300

    feat: add style in NoContent component

--- src/components/NoContent/style.ts
+++ src/components/NoContent/style.ts
@@ -1,5 +1,20 @@
+import theme from '@src/theme';
+import { transparentize } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
   margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${transparentize(0.5, theme.colors.gray[700])};
+
+  svg {
+    margin-bottom: 5px;
+    color: ${transparentize(0.5, theme.colors.gray[700])};
+  }
+
+  @media (max-width: 376px) {
+    text-align: center;
+  }
 `;
