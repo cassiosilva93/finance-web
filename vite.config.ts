23e2b917fdb529322e771a43d051284635a9a887
Cassio Oliveira Silva <cos@vortx.com.br>
2022-08-15 12:06:17 -0300

    fix: adjust in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import codegen from 'vite-plugin-graphql-codegen';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), codegen()],
+  plugins: [react(), tsconfigPaths()],
 });
