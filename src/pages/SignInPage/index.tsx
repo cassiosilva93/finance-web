eaf6853e23c94a0bf1a822454d66e93f8b8e1f3a
intwone <cassiointw1993@gmail.com>
2022-08-29 15:29:48 -0300

    feat: add loading in signin button when user click in enter

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -1,3 +1,4 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import Button from '@src/components/Button';
 import Input from '@src/components/Input';
 import { useAuth } from '@src/hooks/auth/auth';
@@ -5,8 +6,10 @@ import theme from '@src/theme';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { AiFillEye, AiFillEyeInvisible, AiFillRocket } from 'react-icons/ai';
+import ReactLoading from 'react-loading';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { schema } from './schema';
 import { Container, Form, NoHaveRegister } from './style';
 import FormProps from './types';
 
@@ -14,12 +17,19 @@ export default function SignInPage() {
   const [eyeIsOpen, setEyeIsOpen] = useState(false);
   const navigate = useNavigate();
   const { signIn } = useAuth();
-  const { handleSubmit, register } = useForm<FormProps>({
+  const {
+    handleSubmit,
+    register,
+    formState: { isDirty, isValid, isSubmitting },
+  } = useForm<FormProps>({
+    resolver: yupResolver(schema),
     mode: 'onChange',
   });
 
-  const eyeIcon = eyeIsOpen ? AiFillEye : AiFillEyeInvisible;
-  const typePasswordInput = eyeIsOpen ? 'text' : 'password';
+  const passwordInputState = {
+    eyeIcon: eyeIsOpen ? AiFillEye : AiFillEyeInvisible,
+    typePasswordInput: eyeIsOpen ? 'text' : 'password',
+  };
 
   async function onSubmit({ email, password }: FormProps) {
     const result = await signIn({ email, password });
@@ -38,16 +48,24 @@ export default function SignInPage() {
         <Input type="email" id="email" title="E-mail" register={register} />
 
         <Input
-          type={typePasswordInput}
+          type={passwordInputState.typePasswordInput}
           id="password"
-          icon={eyeIcon}
+          icon={passwordInputState.eyeIcon}
           title="Senha"
           changeStateCallback={setEyeIsOpen}
           register={register}
         />
 
-        <Button type="submit" style={{ height: 40 }}>
-          Entrar
+        <Button
+          type="submit"
+          style={{ height: 40 }}
+          disabled={isSubmitting || !isDirty || !isValid}
+        >
+          {isSubmitting ? (
+            <ReactLoading type="spin" height={20} width={20} />
+          ) : (
+            'Entrar'
+          )}
         </Button>
 
         <a href="#" className="forgot-password">
