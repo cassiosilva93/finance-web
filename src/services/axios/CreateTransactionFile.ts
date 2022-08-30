5d032bf5263172bea9c604c9ec717e785564a680
intwone <cassiointw1993@gmail.com>
2022-08-30 17:21:58 -0300

    chore: remove token mocked

--- src/services/axios/CreateTransactionFile.ts
+++ src/services/axios/CreateTransactionFile.ts
@@ -6,11 +6,5 @@ export async function createTransactionFile(file: FileProps): Promise<void> {
   const fileTyped = file as File;
   data.append('transactions', fileTyped);
 
-  await axiosClient.post('/file', data, {
-    headers: {
-      // TODO: Get auth token and add in Authorization prop
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiZDUzNGM3NTctOTA4Zi00NDliLTllZTktYTNhOTQzNmU5ZjQ4IiwibmFtZSI6IkpvYW8gRmVycmVpcmEiLCJlbWFpbCI6ImpvYW9AZ21haWwuY29tIn0sImlhdCI6MTY2MDU3MzA1NiwiZXhwIjoxNjYxMDA1MDU2fQ.G6GwkvdG3GO2bUayqbLqdIUonTR2W4VbsAkGLD9qhj0',
-    },
-  });
+  await axiosClient.post('/file', data);
 }
--- src/services/clients/apolloClient.ts
+++ src/services/clients/apolloClient.ts
@@ -12,8 +12,7 @@ const httpLink = new HttpLink({
 });
 
 const authMiddleware = new ApolloLink((operation, forward) => {
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiZDUzNGM3NTctOTA4Zi00NDliLTllZTktYTNhOTQzNmU5ZjQ4IiwibmFtZSI6IkpvYW8gRmVycmVpcmEiLCJlbWFpbCI6ImpvYW9AZ21haWwuY29tIn0sImlhdCI6MTY2MDU3MzA1NiwiZXhwIjoxNjYxMDA1MDU2fQ.G6GwkvdG3GO2bUayqbLqdIUonTR2W4VbsAkGLD9qhj0';
+  const token = localStorage.getItem('@financeweb:token');
 
   operation.setContext(({ headers = {} }) => ({
     headers: {
